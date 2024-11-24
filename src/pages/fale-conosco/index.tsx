5976c00e3e709f3bbdde294d61bba2f40bd57a63
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-11-24 18:58:14 -0300

    feat: Inclui página de fale-conosco

--- src/pages/fale-conosco/index.tsx
+++ src/pages/fale-conosco/index.tsx
@@ -4,8 +4,35 @@ function Contato() {
   return (
     <>
       <Layout>
-        <main className="container mx-auto">
-          <h1 className="text-lg font-bold">Fale Conosco</h1>
+        <main className="container mx-auto px-4 py-12">
+          <section className="max-w-2xl leading-relaxed">
+            <h2 className="mb-4 text-2xl font-bold">Fale Conosco</h2>
+            <p className="mb-4">
+              Este espaço é destinado para entrar em contato com a equipe por
+              trás deste site, do projeto e das pesquisas realizadas. Por favor,
+              tenha em mente que{" "}
+              <strong>
+                não realizamos atendimentos diretos, nem recebemos denúncias ou
+                relatos de violência.
+              </strong>
+            </p>
+            <p className="mb-4">
+              Se você é vítima de violência ou deseja reportar alguma situação,
+              recomendamos buscar diretamente os canais oficiais de denúncia ou
+              os equipamentos que compõem a rede de atendimento às mulheres em
+              sua região, que estão disponibilizados no site.
+            </p>
+            <p>
+              Para entrar em contato conosco sobre questões relacionadas ao site
+              ou ao projeto, envie um e-mail para:{" "}
+              <a
+                href="mailto:contato@exemplo.com"
+                className="text-primary underline"
+              >
+                contato@exemplo.com
+              </a>
+            </p>
+          </section>
         </main>
       </Layout>
     </>
