1c5bd40eb580e30b75e2d7a7dd18764e745ac39e
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-11-27 15:20:42 -0300

    refactor: Atualizar a estrutura de dados e a funcionalidade de busca na página ExplorarDados.

--- src/constants/dados.ts
+++ src/constants/dados.ts
@@ -1,38 +1,206 @@
-interface DataItem {
-  name: string;
-  phone: string;
-  location: string;
+export interface ServiceData {
+  endereco?: string;
+  telefone?: string;
+  email?: string;
 }
 
-export const dados: DataItem[] = [
+export interface CityData {
+  cidade: string;
+  policia_militar: ServiceData;
+  policia_civil: ServiceData | null;
+  cras: ServiceData | ServiceData[];
+  creas: ServiceData | null;
+  hospitais_upa: ServiceData | null;
+}
+
+export const cityData: CityData[] = [
   {
-    name: "Centro de Referência da Mulher",
-    phone: "(33) 1234-5678",
-    location: "Araçuaí",
+    cidade: "Araçuaí",
+    policia_militar: {
+      endereco: "R. Juíz de Paz Pedro de Carvalho, 177",
+      telefone: "190",
+    },
+    policia_civil: {
+      endereco: "Praça Rui Barbosa, 26 - Centro",
+      telefone: "197",
+    },
+    cras: [
+      {
+        endereco: "R. Vitória, 47 - Nova Terra",
+        telefone: "33 3731-2511",
+      },
+      {
+        endereco: "Av. Dr. Nuno Melo, 78 - Centro",
+      },
+    ],
+    creas: {
+      endereco: "R. São Geraldo, 726 - Planalto",
+      email: "ad.sagi@mds.gov.br",
+    },
+    hospitais_upa: {
+      endereco: "R. Amazonas, 255 - Nova Terra",
+      telefone: "33 3731-1333",
+    },
   },
   {
-    name: "Delegacia Especializada",
-    phone: "(33) 2345-6789",
-    location: "Diamantina",
+    cidade: "Berilo",
+    policia_militar: {
+      endereco: "Av. Juscelino Kubitschek, 648",
+      telefone: "190",
+    },
+    policia_civil: {
+      endereco: "Av. JK, 107 - Planaltino",
+      telefone: "197",
+    },
+    cras: {
+      endereco: "R. Vereador Teodoro Vieira de Souza, 1 - Dom Silvestre",
+      email: "crasberilomg@hotmail.com",
+    },
+    creas: null,
+    hospitais_upa: {
+      endereco: "R. Padre Pedro Heredes, S/N - São Francisco",
+      telefone: "33 3737-1175",
+    },
   },
   {
-    name: "Delegacia Especializada",
-    phone: "(33) 2345-6789",
-    location: "Diamantina",
+    cidade: "Coronel Murta",
+    policia_militar: {
+      endereco: "Av. Celso Murta, 115",
+      telefone: "190",
+    },
+    policia_civil: {
+      endereco: "R. Rita Murta, 117 - Centro",
+      telefone: "33 3751-1296",
+    },
+    cras: {
+      endereco: "R. Severo Leão, 63 - Itaporé Velho",
+      email: "coronelmurta.mg.gov.br",
+    },
+    creas: null,
+    hospitais_upa: {
+      endereco: "R. Lufa - Acari",
+      telefone: "33 3735-1284",
+    },
   },
   {
-    name: "Delegacia Especializada",
-    phone: "(33) 2345-6789",
-    location: "Diamantina",
+    cidade: "Francisco Badaró",
+    policia_militar: {
+      endereco: "R. Nova",
+      telefone: "33 3738-1190",
+    },
+    policia_civil: {
+      endereco: "R. Direita, S/Nº - Centro",
+      telefone: "33 3764-1170",
+    },
+    cras: {
+      endereco: "R. Costa Melo, 10 - Centro",
+      email: "cras@franciscobadaro.mg.gov.br",
+    },
+    creas: null,
+    hospitais_upa: {
+      endereco: "Av. Santa Cruz, 488",
+      telefone: "33 3733-1589",
+    },
   },
   {
-    name: "Delegacia Especializada",
-    phone: "(33) 2345-6789",
-    location: "Diamantina",
+    cidade: "Itinga",
+    policia_militar: {
+      endereco: "R. João Menezes, 301",
+      telefone: "190",
+    },
+    policia_civil: {
+      endereco: "Av. Marechal Rondon, 191 - Centro",
+      telefone: "197",
+    },
+    cras: {
+      endereco: "Av. Brasil, 5 - Centro",
+      email: "cras@itinga.mg.gov.br",
+    },
+    creas: null,
+    hospitais_upa: {
+      endereco: "Av. Santa Cruz, 488 - Planalto",
+      telefone: "33 3733-1589",
+    },
   },
   {
-    name: "Delegacia Especializada",
-    phone: "(33) 2345-6789",
-    location: "Diamantina",
+    cidade: "Jenipapo de Minas",
+    policia_militar: {
+      endereco: "R. Setúbal, 352",
+      telefone: "190",
+    },
+    policia_civil: null,
+    cras: {
+      endereco: "R. Pouso, 277 - Lagoinha",
+      email: "crasjenipa@yahoo.com.br",
+      telefone: "98 8267-89698",
+    },
+    creas: null,
+    hospitais_upa: {
+      endereco: "R. Costa, 1020 - Lagoinha",
+      telefone: "33 3738-9237",
+    },
+  },
+  {
+    cidade: "José Gonçalves de Minas",
+    policia_militar: {
+      endereco: "R. Ana Santos Carvalho, 34",
+      telefone: "190",
+    },
+    policia_civil: null,
+    cras: {
+      endereco: "R. Erestal de Fátima Sales, 26 - Centro",
+      email: "crasjgm2017@gmail.com",
+    },
+    creas: null,
+    hospitais_upa: null,
+  },
+  {
+    cidade: "Virgem da Lapa",
+    policia_militar: {
+      endereco: "Av. Brasil, 370",
+      telefone: "190",
+    },
+    policia_civil: {
+      endereco: "Av. Brasil, 397 - Centro",
+      telefone: "33 3736-1227",
+    },
+    cras: {
+      endereco: "R. Pará, 41 - Novo Horizonte",
+      email: "assistenciasocial@virgemdalapa.mg.gov.br",
+    },
+    creas: null,
+    hospitais_upa: {
+      endereco: "Praça Minas Gerais, 85 - Belo Horizonte",
+      telefone: "33 99824-6930",
+    },
+  },
+  {
+    cidade: "Itaobim",
+    policia_militar: {
+      endereco: "Av. Santo Antonio, 55",
+      telefone: "190",
+    },
+    policia_civil: {
+      endereco:
+        "R. Sen. Lucio Bittencourt, Av. Santo Antônio, 350 - Bairro Santo Antonio",
+      telefone: "197",
+    },
+    cras: [
+      {
+        endereco: "17 de Julho, 247 - Vila Nova",
+        email: "social@itaobim.mg.gov.br",
+      },
+      {
+        endereco: "R. Max Machado, S/N - Centro",
+        email: "social@itaobim.mg.gov.br",
+      },
+    ],
+    creas: {
+      endereco: "Rua Piaui, 231 – Centro",
+    },
+    hospitais_upa: {
+      endereco: "R. Minas Novas, 387 - São Jorge",
+      telefone: "33 3734-1258",
+    },
   },
 ];
--- src/pages/explorar-dados/index.tsx
+++ src/pages/explorar-dados/index.tsx
@@ -1,72 +1,144 @@
-import Layout from "@/components/layouts";
 import React, { useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { dados } from "@/constants/dados";
+import { cityData, ServiceData } from "@/constants/dados";
+import Layout from "@/components/layouts";
+import { BadgeIcon as Police, Building2, Hospital, Search } from "lucide-react";
 
-function ExplorarDados() {
+export default function ExplorarDados() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [data, setData] = useState(dados);
+
+  const filteredData = cityData.filter((item) =>
+    item.cidade.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const term = event.target.value.toLowerCase();
-    setSearchTerm(term);
-    const filteredData = dados.filter(
-      (item) =>
-        item.name.toLowerCase().includes(term) ||
-        item.location.toLowerCase().includes(term),
-    );
-    setData(filteredData);
+    setSearchTerm(event.target.value);
   };
-  return (
-    <>
-      <Layout>
-        <main className="container mx-auto px-4 py-8">
-          <h1 className="mb-6 text-3xl font-bold">Explorar dados</h1>
 
-          <p className="mb-6 text-muted-foreground">
-            Você pode encontrar dados sobre sua região usando o mapa abaixo ou
-            pesquisando na lista de serviços disponíveis.
+  const renderServiceData = (data: ServiceData | ServiceData[] | null) => {
+    if (!data) return <p>Não disponível</p>;
+    if (Array.isArray(data)) {
+      return data.map((item, index) => (
+        <div key={index} className="mb-2">
+          {item.endereco && (
+            <p>
+              <strong>Endereço:</strong> {item.endereco}
+            </p>
+          )}
+          {item.telefone && (
+            <p>
+              <strong>Telefone:</strong> {item.telefone}
+            </p>
+          )}
+          {item.email && (
+            <p>
+              <strong>Email:</strong> {item.email}
+            </p>
+          )}
+        </div>
+      ));
+    }
+    return (
+      <>
+        {data.endereco && (
+          <p>
+            <strong>Endereço:</strong> {data.endereco}
           </p>
+        )}
+        {data.telefone && (
+          <p>
+            <strong>Telefone:</strong> {data.telefone}
+          </p>
+        )}
+        {data.email && (
+          <p>
+            <strong>Email:</strong> {data.email}
+          </p>
+        )}
+      </>
+    );
+  };
 
-          <div className="mb-8">
-            <iframe
-              src="https://qgiscloud.com/joaojacintho/efas/?l=Escola%20Fam%C3%ADlia%20Agr%C3%ADcola%2CMinas%20Gerais&bl=Aerial&t=efas&e=-5612597%2C-3321934%2C-4412240%2C-1083100"
-              width="100%"
-              height="600"
-              className="rounded border"
-            ></iframe>
-          </div>
+  return (
+    <Layout>
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="mb-6 text-3xl font-bold text-primary">Explorar dados</h1>
 
+        <p className="mb-6 text-muted-foreground">
+          Você pode encontrar dados sobre sua região usando o mapa abaixo ou
+          pesquisando na lista de serviços disponíveis.
+        </p>
+
+        <div className="mb-8">
+          <iframe
+            src="https://qgiscloud.com/joaojacintho/efas/?l=Escola%20Fam%C3%ADlia%20Agr%C3%ADcola%2CMinas%20Gerais&bl=Aerial&t=efas&e=-5612597%2C-3321934%2C-4412240%2C-1083100"
+            width="100%"
+            height="600"
+            className="rounded border"
+          ></iframe>
+        </div>
+
+        <div className="relative mb-6">
+          <Search className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 transform text-gray-400" />
           <Input
             type="text"
-            placeholder="Pesquisar por nome ou localização"
+            placeholder="Pesquisar por cidade"
             value={searchTerm}
             onChange={handleSearch}
-            className="mb-6"
+            className="pl-8"
           />
+        </div>
 
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            {data.map((item, index) => (
-              <Card key={index}>
-                <CardHeader>
-                  <CardTitle>{item.name}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <p>
-                    <strong>Telefone:</strong> {item.phone}
-                  </p>
-                  <p>
-                    <strong>Localização:</strong> {item.location}
-                  </p>
-                </CardContent>
-              </Card>
-            ))}
-          </div>
-        </main>
-      </Layout>
-    </>
+        <div className="grid grid-cols-1 gap-6">
+          {filteredData.map((item, index) => (
+            <Card key={index}>
+              <CardHeader>
+                <CardTitle>{item.cidade}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+                  <div>
+                    <h3 className="mb-2 flex items-center font-bold">
+                      <Police className="mr-2 h-5 w-5" />
+                      Polícia Militar
+                    </h3>
+                    {renderServiceData(item.policia_militar)}
+                  </div>
+                  <div>
+                    <h3 className="mb-2 flex items-center font-bold">
+                      <Police className="mr-2 h-5 w-5" />
+                      Polícia Civil
+                    </h3>
+                    {renderServiceData(item.policia_civil)}
+                  </div>
+                  <div>
+                    <h3 className="mb-2 flex items-center font-bold">
+                      <Building2 className="mr-2 h-5 w-5" />
+                      CRAS
+                    </h3>
+                    {renderServiceData(item.cras)}
+                  </div>
+                  <div>
+                    <h3 className="mb-2 flex items-center font-bold">
+                      <Building2 className="mr-2 h-5 w-5" />
+                      CREAS
+                    </h3>
+                    {renderServiceData(item.creas)}
+                  </div>
+                  <div>
+                    <h3 className="mb-2 flex items-center font-bold">
+                      <Hospital className="mr-2 h-5 w-5" />
+                      Hospital/UPA
+                    </h3>
+                    {renderServiceData(item.hospitais_upa)}
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </Layout>
   );
 }
-
-export default ExplorarDados;
