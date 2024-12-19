7e7f702bf0518cd1820083ef66d45f4cd1067343
Afranio Caires <afraniomcaires@gmail.com>
2024-12-18 21:59:34 -0300

    fix: inclui dados restantes

--- src/constants/dados.ts
+++ src/constants/dados.ts
@@ -203,4 +203,217 @@ export const cityData: CityData[] = [
       telefone: "33 3734-1258",
     },
   },
+  {
+    cidade: "Monte Formoso",
+    policia_militar: {
+      endereco: "R. José Pinheiro, 292",
+      telefone: "190",
+    },
+    policia_civil: null,
+    cras: {
+      endereco: "Alvaro Moura, 36 - centro",
+      email: "Cras.pmmf@gmail.com",
+    },
+    creas: null,
+    hospitais_upa: null,
+  },
+  {
+    cidade: "Novo Cruzeiro",
+    policia_militar: {
+      endereco: "Av. Julio Campos, 219",
+      telefone: "190",
+    },
+    policia_civil: {
+      endereco: "R. Nice Maravilha, 43 A - São Bento",
+      telefone: "33 3533 1412",
+    },
+    cras: {
+      endereco: "R. Jorge Chaim, 415 - Anastacio Roque",
+      email: "crassedenc@gmail.com",
+    },
+    creas: {
+      endereco: "R. Nice Maravilha , 53 - São Bento",
+      email: "aiad.sagi@mds.gov.br",
+    },
+    hospitais_upa: {
+      endereco: "Praça Frei Ubaldo, 47 - Centro",
+      telefone: "33 3533-1934",
+    },
+  },
+  {
+    cidade: "Padre Paraíso",
+    policia_militar: {
+      endereco: "R. Amazonas, 94",
+      telefone: "190",
+    },
+    policia_civil: {
+      endereco: "R. Santa Luzia, 14 - Centro",
+      telefone: "33 3534 1211",
+    },
+    cras: {
+      endereco: "R. Olimpo Duarte, 180 - Centro",
+      email: "cras@padreparaiso.mg.gov.br",
+    },
+    creas: {
+      endereco: "Rua Aracuai, 259 – Centro",
+    },
+    hospitais_upa: {
+      endereco: "R. Doná Áurea, 2",
+      telefone: "33 3534-1210",
+    },
+  },
+  {
+    cidade: "Ponto dos Volantes",
+    policia_militar: {
+      endereco: "R. da Matriz, 126",
+      telefone: "190",
+    },
+    policia_civil: null,
+    cras: {
+      endereco: "R. CURITIBA, 205, CENTRO",
+      email: "cras.pvolantes@gmail.com",
+    },
+    creas: {
+      endereco: "R. Nova, 0 - Centro",
+    },
+    hospitais_upa: null,
+  },
+  {
+    cidade: "Águas Vermelhas",
+    policia_militar: {
+      endereco: "Praça Mal. Castelo Branco, 282",
+      telefone: "190",
+    },
+    policia_civil: {
+      endereco: "R: São Vicente, 215 -Centro",
+      telefone: "33 3755 1203",
+    },
+    cras: {
+      endereco: "R. Sao Vicente, 52- Centro",
+      email: "crasaguasv@gmail.com",
+    },
+    creas: null,
+    hospitais_upa: {
+      endereco: "R. Uberlândia, S/N",
+      telefone: "33 3755-1201",
+    },
+  },
+  {
+    cidade: "Cachoeira de Pajéu",
+    policia_militar: {
+      endereco: "Praça Doutor Freire, 88",
+      telefone: "33 3754 1273",
+    },
+    policia_civil: {
+      endereco: "Praça Rui Barbosa, 150 -Centro",
+      telefone: "33 3751 1400",
+    },
+    cras: {
+      endereco: "R. Propércio de Almeida, 16 - Centro",
+      email: "smas_cpajeu@yahoo.com.br",
+    },
+    creas: null,
+    hospitais_upa: {
+      endereco: "Praça Rui Barbosa, S/N - Centro",
+    },
+  },
+  {
+    cidade: "Comercinho",
+    policia_militar: {
+      endereco: "R. Joaquim José dos Reis",
+      telefone: "190",
+    },
+    policia_civil: null,
+    cras: {
+      endereco: "R. Manoel Rafael Oliveira, 222 - Centro",
+      email: "crascomercinho@gmail.com",
+    },
+    creas: null,
+    hospitais_upa: null,
+  },
+  {
+    cidade: "Divisa Alegre",
+    policia_militar: {
+      endereco: "Av. Ver. Cláudio Alves da Costa",
+      telefone: "33 99914 4015",
+    },
+    policia_civil: {
+      endereco: "R. Vereador Cláudio Alves Costa, 1314 - Centro",
+      telefone: "197",
+    },
+    cras: {
+      endereco: "João Meira dos Santos, 1999, Centro",
+      email: "crasdivisaalegre2021@gmail.com",
+    },
+    creas: {
+      endereco: "R. João Meira dos santos, 1643 - Centro",
+    },
+    hospitais_upa: {
+      endereco: "Praça da Liberdade, 247",
+      telefone: "33 3755-8448",
+    },
+  },
+  {
+    cidade: "Divisópolis",
+    policia_militar: {
+      endereco: "Av. Vicente Pereira, 994-996",
+      telefone: "190",
+    },
+    policia_civil: null,
+    cras: {
+      endereco: "R. Firmino da Silva Nunes, 318",
+      email: "crasdivisopolis@hotmail.com",
+    },
+    creas: {
+      endereco: "R. Hélio Ferraz, 1081 – Centro",
+    },
+    hospitais_upa: {
+      endereco: "R. Fetaemg, S/N - centro",
+      telefone: "33 98848-2258",
+    },
+  },
+  {
+    cidade: "Medina",
+    policia_militar: {
+      endereco: "R. Francisco Figueiredo",
+      telefone: "190",
+    },
+    policia_civil: {
+      endereco: "Rua Francisco Figueiredo, S/N°-Centro",
+      telefone: "33 3753 1110",
+    },
+    cras: {
+      endereco: "R. Belo Horizonte 389 - Centro",
+      email: "cras3medina@hotmail.com",
+    },
+    creas: {
+      endereco: "Av. Valdivio Antunes Guimarães, 50 - Cidade Jardim",
+    },
+    hospitais_upa: {
+      endereco: "R. Dr. Geraldo Magela, 180",
+      telefone: "33 3753-1334",
+    },
+  },
+  {
+    cidade: "Pedra Azul",
+    policia_militar: {
+      endereco: "R. Hormínio Alemida, 216",
+      telefone: "33 98761 9354",
+    },
+    policia_civil: {
+      endereco: "AV. CASSIANO MENDES, 366-Centro",
+      telefone: "33 3751 1400",
+    },
+    cras: {
+      endereco: "Av. Joaquim Antunes, 67 - Centro",
+      email: "craspedraazul@hotmail.com",
+    },
+    creas: {
+      endereco: "Av. Joaquim Antunes, 67 – Centro",
+    },
+    hospitais_upa: {
+      endereco: "Av. Dr. Álvaro Neves, 369",
+      telefone: "33 3751-1644",
+    },
+  },
 ];
